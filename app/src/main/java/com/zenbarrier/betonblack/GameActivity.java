8f1498bc15a328563f29ab26220044f0fa5b0cb2
ZenBarrier <anthony.j.barrera@gmail.com>
2017-03-09 00:14:31 -0500

    Alertdialog for getting user input cash set. Need logic to store amount.

--- app/src/main/java/com/zenbarrier/betonblack/GameActivity.java
+++ app/src/main/java/com/zenbarrier/betonblack/GameActivity.java
@@ -3,17 +3,29 @@ package com.zenbarrier.betonblack;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
     // Remove the below line after defining your own ad unit ID.
     private static final String TOAST_TEXT = "Test ads are being shown. "
             + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+
+    Strategy mStrategy;
+    int mCash;
+    boolean mJustStarted = true;
 
 
     @Override
@@ -32,9 +44,51 @@ public class GameActivity extends AppCompatActivity {
                 .setRequestAgent("android_studio:ad_template").build();
         adView.loadAd(adRequest);
 
+        Intent intent = getIntent();
+        mStrategy = (Strategy) intent.getSerializableExtra(MainActivity.KEY_STRATEGY);
+
+        getCashDialog();
+
 
         // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
         Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+    }
+
+    private void getCashDialog() {
+        EditText cash = new EditText(this);
+        cash.setInputType(InputType.TYPE_CLASS_NUMBER);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setMessage("How much money are you gambling with?")
+                .setTitle("Starting Cash").setView(cash)
+                .setCancelable(false)
+                .setPositiveButton("Set",null);
+        if(!mJustStarted){
+            builder.setNegativeButton("Cancel", null);
+        }
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button button = ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mJustStarted = false;
+                    }
+                });
+            }
+        });
+        dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_BACK){
+                    if(mJustStarted) finish();
+                    else dialog.dismiss();
+                }
+                return true;
+            }
+        });
+        dialog.show();
     }
 
 
