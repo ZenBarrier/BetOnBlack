34f14b0f56862e8c8a9c54d6def14a7c38a9e1d9
ZenBarrier <anthony.j.barrera@gmail.com>
2017-03-27 20:56:14 -0400

    Removed menu for dialog activity in new strategy activity.

--- app/src/main/java/com/zenbarrier/betonblack/NewStrategyActivity.java
+++ app/src/main/java/com/zenbarrier/betonblack/NewStrategyActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
@@ -48,24 +46,6 @@ public class NewStrategyActivity extends AppCompatActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_strategy, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
-
-    }
 
     public void saveStrategy(View view) {
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
