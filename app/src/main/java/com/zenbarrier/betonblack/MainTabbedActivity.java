a7841b2e47a4d612d956ed79b787340554b0d454
ZenBarrier <anthony.j.barrera@gmail.com>
2017-04-16 17:26:37 -0400

    Removed menu item from main activity.

--- app/src/main/java/com/zenbarrier/betonblack/MainTabbedActivity.java
+++ app/src/main/java/com/zenbarrier/betonblack/MainTabbedActivity.java
@@ -12,8 +12,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
@@ -110,28 +108,6 @@ public class MainTabbedActivity extends AppCompatActivity {
         ((ViewGroup)mAdView.getParent()).removeView(mAdView);
         mAdView.destroy();
         super.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_tabbed, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
