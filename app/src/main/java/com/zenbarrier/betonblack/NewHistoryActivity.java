a4e342cfc617680eacd74ced7bfaa24d81d46a50
ZenBarrier <anthony.j.barrera@gmail.com>
2017-04-16 22:56:18 -0400

    New history layout laid out.

--- app/src/main/java/com/zenbarrier/betonblack/NewHistoryActivity.java
+++ app/src/main/java/com/zenbarrier/betonblack/NewHistoryActivity.java
@@ -2,6 +2,7 @@ package com.zenbarrier.betonblack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewHistoryActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class NewHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_history);
+    }
+
+    public void saveHistory(View view) {
+    }
+
+    public void cancelHistory(View view) {
     }
 }
